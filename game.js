1377c0e0a65366e635f705e45cd4e2e56338d8e1
Nishchhal <nishchhal901@gmail.com>
2022-08-09 11:24:19 +0530

    Sequence to the User with Animations and Sounds Fixed

---
+++ game.js
@@ -0,0 +1,25 @@
+const color = ["red", "blue", "green", "yellow"];
+                //  0      1        2         3
+const sounds = ["sounds/red.mp3","sounds/blue.mp3","sounds/green.mp3","sounds/yellow.mp3","sounds/wrong.mp3"];
+                //        0             1                    2                 3
+const gamePattern = [];
+
+function nextSequence() {
+    
+    var randomNumber = Math.floor(Math.random()*4)
+
+    console.log(randomNumber);
+    
+    //Associated Color with that button
+
+    var randomChosenColor = color[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $(`#${color[randomNumber]}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    //Associated Audio with Buttton
+    var audio = new Audio(`${sounds[randomNumber]}`);
+    // audio.play();
+
+
+}
+nextSequence();
